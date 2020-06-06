2aed4cd8c257a617358425e9ecc4b07e0b122705
ZhangQianYing <1256588196@qq.com>
2020-06-06 23:14:54 +0800

    Mod: 修改线上和内网的接口地址

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -2,7 +2,13 @@ let url, name
 
 if (window.location.href.includes('localhost')) {
     name = 'middle-system'
-    url = 'http://z-refund.djson.cn'
+    url = 'http://k-platform.djson.cn'
+} else if(window.location.href.includes('middle-platform')) {
+    name = 'middle-system-in'
+    url = 'http://k-platform.djson.cn'
+} else if(window.location.href.includes('ocean.dangkou.net')) {
+    name = 'middle-system-net'
+    url = 'http://k-platform.dangkou.net'
 }
 
 let data = {
